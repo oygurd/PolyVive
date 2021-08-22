6b7f9fcde0d160846104ead4c1abcd2600304b2b
Saul <sorusamaa@gmail.com>
2021-08-22 17:13:10 +0300

    Create HealthBar.cs
    Health bar script

---
+++ HealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider; //this is the HP slider
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health; //makes sure that the slider starts at the maximum amount of health.
+    }
+
+
+    public void SetHealth(int health)
+    {
+        slider.value = health; //this will make the slider (HP Bar) be move with the amount of health it current has.
+
+
+    }
+}
